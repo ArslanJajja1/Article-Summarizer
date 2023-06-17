18b8f733af89ae4e8637108f361e7283adc8e407
arslanjajja1 <arslanjajja13@gmail.com>
2023-06-17 16:12:17 +0500

    copy to clipboard feature

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -6,6 +6,7 @@ const Demo = () => {
     const [article, setArticle] = useState({url:'',summary:''})
     const [getSummary,{error,isFetching}] = useLazyGetSummaryQuery()
     const [allArticles,setAllArticles] = useState([])
+    const [copied,setCopied] = useState("")
     useEffect(()=>{
         const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles'))
         if (articlesFromLocalStorage){
@@ -23,6 +24,11 @@ const Demo = () => {
             localStorage.setItem('articles',JSON.stringify(updatedAllArticles))
         }
     }
+    const handleCopy = (copyUrl)=>{
+        setCopied(copyUrl)
+        navigator.clipboard.writeText(copyUrl)
+        setTimeout(()=>setCopied(""),3000)
+    }
   return (
     <section className='mt-16 w-full max-w-full'>
         <div className="flex flex-col w-full gap-2">
@@ -36,8 +42,8 @@ const Demo = () => {
             <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
                 {allArticles.map((item,index)=>(
                     <div key={`link-${index}`} onClick={()=>setArticle(item)} className='link_card'>
-                        <div className="copy__btn">
-                            <img src={copy} alt="copy_icon" className='w-[40%] h-[40%] object-contain'/>
+                        <div className="copy__btn" onClick={()=>handleCopy(item.url)}>
+                            <img src={copied === item.url ? tick : copy} alt="copy_icon" className='w-[40%] h-[40%] object-contain'/>
                         </div>
                         <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>{item.url}</p>
                     </div>
